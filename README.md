81b5c6178d20f85592032dc95bf3826111d4b7dd
Akeem King <akeemtlking@gmail.com>
2018-05-13 11:42:35 -0400

    adding gitignore and todo list

---
+++ README.md
@@ -0,0 +1,5 @@
+*todo
++ Add autocompletion after (.)
++ Add line numbering
++ Add column numbering
++ Add figure out how to successfully scroll page
--- myTerm.c
+++ myTerm.c
@@ -1,8 +1,7 @@
-#include <curses.h>
-#include <stdlib.h>
-#include <signal.h>
 #include <string.h>
 #include <menu.h>
+#include <curses.h>
+#include <stdlib.h>
 
 static void finish(int sig);
 static void autocomplete();
@@ -13,29 +12,42 @@ int main() {
   (void) noecho(); // Disables automatic echoing
   refresh(); // Clears screen entirely
   keypad(stdscr, TRUE); // Captures special keystrokes
-  char buf[500];
+
+  char *word = calloc(500, 1);
   int i = 0;
+  char c; 
   for(;;) {
+	// Capturing user input and displaying it
     char c = getch();
+	refresh();
     addch(c);
+	// If user input a period then we look to autocomplete
     if (c == '.') {
-      autocomplete();
-      i = 0;
-      continue;
+	  int x, y;
+  	  getyx(stdscr, y, x);	  
+      autocomplete(x, y);
+	  addch(' ');
+      continue; // No need to go through rest of loop
     }
-    buf[i] = c;
-    i++;
+   word[i] = c;
+   i++;
   }
-  finish(0);
 }
 
-static void autocomplete() {
+static void autocomplete(const size_t x, const size_t y) {
   ITEM **i = (ITEM **)calloc(2, sizeof(ITEM*));
   i[0] = new_item("Name", "Desc");
   MENU *m = new_menu((ITEM**)i);
+  WINDOW *men_win = derwin(stdscr, 0, 0, x, y);
+  set_menu_sub(m, men_win);
   post_menu(m);
   refresh();
+  free_item(i[0]);
+  free_menu(m); 
+  unpost_menu(m);
+  delwin(men_win);
 }
+
 static void finish(int sig) {
   endwin();
   exit(0);
